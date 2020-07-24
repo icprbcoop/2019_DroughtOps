f694cc97fb1f9cec3af9ff1076f83bcafe324216
cheriels <cschultz@icprb.org>
2020-07-24 09:39:08 -0400

    CS: reformatting ui.R

--- ui.R
+++ ui.R
@@ -4,7 +4,15 @@
 # Run the application by clicking 'Run App' above.
 #--------------------------------------------------------------------------------
 #--------------------------------------------------------------------------------
-dashboardPage(skin = "blue",
+# The structure of ui.R is
+#   dashboardPage(
+#     dashboardHeader(),
+#     dashboardSidebar(),
+#     dashboardBody()
+#   )
+# To debug, can completely comment out 1 or 2 of these.
+
+dashboardPage(skin = "blue" ,
   dashboardHeader(title = "CO-OP Operations",
                   .list = NULL,
                   #this space is for outputting the date to the login bar
@@ -12,104 +20,98 @@ dashboardPage(skin = "blue",
                   #to output properly
                   tags$li(textOutput("date_text"),
                           class = "dropdown")
-                  ),
+                  ), # end dashboardHeader
   #--------------------------------------------------------------------------------
   #--------------------------------------------------------------------------------
   # The LHS sidebar - has the user inputs and controls
   #--------------------------------------------------------------------------------
   #--------------------------------------------------------------------------------
-  dashboardSidebar(
-    width = 250,
-    dateRangeInput("plot_range",
-               "Specify plot range",
-               # start = date_start,
-              start = "2020-05-06",
-              end = "2020-09-30",
-               # start = date_start,
-               # end = date_end,
-               format = "yyyy-mm-dd",
-               width = NULL),
-dateInput("DREXtoday",
-          "Select end date of simulation (> yesterday)",
-          width = "200px",
-          value = date_today0, # date_today0 = today() - set in parameters.R
-#          value = "1930-02-15",
-#          min = "1929-10-02",
-#          max = "1931-12-31",
-          min = date_start,
-          max = date_end,
-          format = "yyyy-mm-dd"),
-
-actionButton("run_main",
-             "Run simulation",
-             icon = NULL,
-             width = "220px"),
-br(),
-numericInput("chunkofdays",
-             "Chunk of days",
-             value = 7,
-             min = 1,
-             max = 30,
-             width = "220px"),
-actionButton("run_add",
-             "Add chunk of days to simulation",
-             icon = NULL,
-             width = "220px"),
-br(),
-numericInput("dr_va",
-             "Demand reduction, VA-Shenandoah (%)",
-             value = dr_va0,
-             min = 0,
-             max = 20,
-             width = "220px"),
-numericInput("dr_md_cent",
-             "Demand reduction, MD-Central (%)",
-             value = dr_md_cent0,
-             min = 0,
-             max = 20,
-             width = "220px"),
-numericInput("dr_md_west",
-             "Demand reduction, MD-Western (%)",
-             value = dr_md_west0,
-             min = 0,
-             max = 20,
-             width = "220px"),
-br(),
-numericInput("mos_1day",
-             "1 day margin of safety (MGD)",
-             value = mos_1day0,
-             min = 0,
-             max = 140,
-             width = "220px"),
-numericInput("dr_wma_override",
-             "Demand reduction override, WMA (%)",
-             value = dr_wma_override0,
-             min = 0,
-             max = 20,
-             width = "220px"),
-br(), br(),
-br(), br(),
-actionButton("write_ts",
-             "Write output time series",
-             icon = NULL,
-             width = "220px")
-      ),
+  dashboardSidebar(width = 250,
+                   dateRangeInput("plot_range",
+                                  "Specify plot range",
+                                  # start = date_start,
+                                  start = "2020-05-06",
+                                  end = "2020-09-30",
+                                  # start = date_start,
+                                  # end = date_end,
+                                  format = "yyyy-mm-dd",
+                                  width = NULL),
+                   dateInput("DREXtoday",
+                             "Select end date of simulation (> yesterday)",
+                             width = "200px",
+                             # value = "1930-02-15",
+                             value = date_today0,
+                             # date_today0 = today() - set in parameters.R
+                             min = date_start,
+                             max = date_end,
+                             format = "yyyy-mm-dd"),
+                   actionButton("run_main",
+                                "Run simulation",
+                                icon = NULL,
+                                width = "220px"),
+                   br(),
+                   numericInput("chunkofdays",
+                                "Chunk of days",
+                                value = 7,
+                                min = 1,
+                                max = 30,
+                                width = "220px"),
+                   actionButton("run_add",
+                                "Add chunk of days to simulation",
+                                icon = NULL,
+                                width = "220px"),
+                   br(),
+                   numericInput("dr_va",
+                                "Demand reduction, VA-Shenandoah (%)",
+                                value = dr_va0,
+                                min = 0,
+                                max = 20,
+                                width = "220px"),
+                   numericInput("dr_md_cent",
+                                "Demand reduction, MD-Central (%)",
+                                value = dr_md_cent0,
+                                min = 0,
+                                max = 20,
+                                width = "220px"),
+                   numericInput("dr_md_west",
+                                "Demand reduction, MD-Western (%)",
+                                value = dr_md_west0,
+                                min = 0,
+                                max = 20,
+                                width = "220px"),
+                   br(),
+                   numericInput("mos_1day",
+                                "1 day margin of safety (MGD)",
+                                value = mos_1day0,
+                                min = 0,
+                                max = 140,
+                                width = "220px"),
+                   numericInput("dr_wma_override",
+                                "Demand reduction override, WMA (%)",
+                                value = dr_wma_override0,
+                                min = 0,
+                                max = 20,
+                                width = "220px"),
+                   br(), br(),
+                   br(), br(),
+                   actionButton("write_ts",
+                                "Write output time series",
+                                icon = NULL,
+                                width = "220px")
+                   ), # end dashboardSidebar
 #--------------------------------------------------------------------------------
 #--------------------------------------------------------------------------------
 # The body - has the graphs and other output info
 #--------------------------------------------------------------------------------
 #--------------------------------------------------------------------------------
   dashboardBody(
-    tags$head(
-      #this links the shiny app to main.css which can be used to easily define and 
-      #alter styles(color,font size, alignments) across allui/server elements
-      tags$link(rel = "stylesheet", type = "text/css", href = "CSS/main.css")),
-    
-    
-      # navbarMenu(title = "where to?", menuName = title, icon = NULL), 
-      navbarPage(title=NULL #,
+    # tags$head(
+    #   #this links the shiny app to main.css which can be used to easily define and 
+    #   #alter styles(color,font size, alignments) across allui/server elements
+    #   tags$link(rel = "stylesheet", type = "text/css", href = "CSS/main.css")),
+    # # navbarMenu(title = "where to?", menuName = title, icon = NULL), 
+    # navbarPage(title=NULL #,
 
-    
       # source("code/ui/situational_awareness_ui.R", local = TRUE)$value,
       # # source("code/ui/one_day_ops_ui.R", local = TRUE)$value,
       # source("code/ui/ten_day_ops_ui.R", local = TRUE)$value,
@@ -118,8 +120,6 @@ actionButton("write_ts",
       # source("code/ui/simulation_ui.R", local = TRUE)$value,
       # source("code/ui/download_data_ui.R", local = TRUE)$value
       
-      
-      )
     ) # end dashboardBody
 ) # end dashboardPage
 
